3a78bcc9952846d0ce6496fdfecfccbe01eb3df3
Axel2007-07 <axelaleman2007@gmail.com>
2025-11-13 20:22:24 -0300

    Punto Items hecho

---
+++ Grupo 12/js/validaciones.js
@@ -0,0 +1,111 @@
+
+const categoriaUno = document.getElementById("categoria00-item01");
+
+categoriaUno.addEventListener("mouseover", function () {
+    categoriaUno.classList.add("resaltado");
+});
+
+categoriaUno.addEventListener("mouseout", function () {
+    categoriaUno.classList.remove("resaltado");
+});
+
+const categoriaDos = document.getElementById("categoria00-item02");
+
+categoriaDos.addEventListener("mouseover", function () {
+    categoriaDos.classList.add("resaltado");
+});
+
+categoriaDos.addEventListener("mouseout", function () {
+    categoriaDos.classList.remove("resaltado");
+});
+const categoriaTres = document.getElementById("categoria00-item03");
+
+categoriaTres.addEventListener("mouseover", function () {
+    categoriaTres.classList.add("resaltado");
+});
+
+categoriaTres.addEventListener("mouseout", function () {
+    categoriaTres.classList.remove("resaltado");
+});
+
+const categoriaCuatro = document.getElementById("categoria00-item04");
+
+categoriaCuatro.addEventListener("mouseover", function () {
+    categoriaCuatro.classList.add("resaltado");
+});
+
+categoriaCuatro.addEventListener("mouseout", function () {
+    categoriaCuatro.classList.remove("resaltado");
+});
+const categoriaCinco = document.getElementById("categoria00-item05");
+
+categoriaCinco.addEventListener("mouseover", function () {
+    categoriaCinco.classList.add("resaltado");
+});
+
+categoriaCinco.addEventListener("mouseout", function () {
+    categoriaCinco.classList.remove("resaltado");
+});
+const categoriaSeis = document.getElementById("categoria00-item06");
+
+categoriaSeis.addEventListener("mouseover", function () {
+    categoriaSeis.classList.add("resaltado");
+});
+
+categoriaSeis.addEventListener("mouseout", function () {
+    categoriaSeis.classList.remove("resaltado");
+});
+const categoriaSiete = document.getElementById("categoria00-item07");
+
+categoriaSiete.addEventListener("mouseover", function () {
+    categoriaSiete.classList.add("resaltado");
+});
+
+categoriaSiete.addEventListener("mouseout", function () {
+    categoriaSiete.classList.remove("resaltado");
+});
+const categoriaOcho = document.getElementById("categoria00-item08");
+
+categoriaOcho.addEventListener("mouseover", function () {
+    categoriaOcho.classList.add("resaltado");
+});
+
+categoriaOcho.addEventListener("mouseout", function () {
+    categoriaOcho.classList.remove("resaltado");
+});
+const categoriaNueve = document.getElementById("categoria00-item09");
+
+categoriaNueve.addEventListener("mouseover", function () {
+    categoriaNueve.classList.add("resaltado");
+});
+
+categoriaNueve.addEventListener("mouseout", function () {
+    categoriaNueve.classList.remove("resaltado");
+});
+const categoriaDiez = document.getElementById("categoria00-item10");
+
+categoriaDiez.addEventListener("mouseover", function () {
+    categoriaDiez.classList.add("resaltado");
+});
+
+categoriaDiez.addEventListener("mouseout", function () {
+    categoriaDiez.classList.remove("resaltado");
+});
+const categoriaOnce = document.getElementById("categoria00-item11");
+
+categoriaOnce.addEventListener("mouseover", function () {
+    categoriaOnce.classList.add("resaltado");
+});
+
+categoriaOnce.addEventListener("mouseout", function () {
+    categoriaOnce.classList.remove("resaltado");
+});
+const categoriaDoce = document.getElementById("categoria00-item12");
+
+categoriaDoce.addEventListener("mouseover", function () {
+    categoriaDoce.classList.add("resaltado");
+});
+
+categoriaDoce.addEventListener("mouseout", function () {
+    categoriaDoce.classList.remove("resaltado");
+});
